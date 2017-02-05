50bac35b9bf4ded80712ebd3b0cae10ecfa69320
kottov <kottov@gmail.com>
2017-02-05 13:39:29 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
